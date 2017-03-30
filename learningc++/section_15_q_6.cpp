0fd0603a33f1ba3a46da058f1d4bad8bdc965946
rohin-arka <surya_siwakoti@outlook.com>
2017-03-30 20:46:47 +0545

    practise switch case in c++

---
+++ learningc++/section_15_q_6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int number1 = 1, number2;
+  cout << "Enter number of your choice" << endl;
+
+  cin >> number2;
+
+  switch(number1){
+    case 1:
+      cout << "Happy";
+    case 2:
+      if (number2 < 0)
+        cout << "Holidays";
+      if (number > 10;)
+        cout << "Weekend"
+
+    default:
+      cout << "Day";
+  }
+  return 0;
+}
