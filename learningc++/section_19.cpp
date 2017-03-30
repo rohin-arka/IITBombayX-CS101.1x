89b275342e97c06bce36093e026dac88393815aa
rohin-arka <surya_siwakoti@outlook.com>
2017-03-30 19:48:03 +0545

    completed section 19 exercises

---
+++ learningc++/section_19.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int date, time, calendarMonth, calendarDate, calendarYear;
+
+  cout << "Enter date in format (dd/mm/yyyy) and time in hh:ss" << endl;
+
+  cin >> date >> time;
+
+  calendarYear = date % 10000;
+
+  calendarDateAndMonth = date /10000;
+
+  calendarDay = calendarDateAndMonth % 100;
+
+  calendarDate = calendarDateAndMonth / 100;
+
+  if ((calendarDate < 31 || calendarDate > 0) && (calendarMonth < 13) && (calendarYear > 2014)){
+    cout << "format is ok" << endl;
+  }
+  else{
+    cout << "Bad format" << endl;
+  }
+
+}
