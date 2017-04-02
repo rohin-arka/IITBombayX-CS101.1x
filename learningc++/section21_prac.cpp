05a5adc88fb301450c287c046978da12beeac105
rohin-arka <surya_siwakoti@outlook.com>
2017-04-02 21:10:47 +0545

    complete while loop practise session

---
+++ learningc++/section21_prac.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main(void){
+  int max, min, avg, numberOfStudents, marks, count = 1, flag = 1, sum = 0;
+  cout << "Enter the number of marks" << endl;
+  cin >> numberOfStudents;
+  while(count <= numberOfStudents){
+    cout << "Enter the marks of student" << endl;
+    cin >> marks;
+    sum += marks;
+    if (flag){
+      min = marks;
+      max = marks;
+      flag = 0;
+    }else{
+      min = marks < min ? marks : min;
+      max = marks > max ? marks : min;
+    }
+    count++;
+  }
+  cout << "The sum of total student marks is ";
+  cout << sum << endl;
+  cout << "The average marks is ";
+  avg = sum / numberOfStudents;
+  cout << avg << endl;
+  cout << "The minimum marks is ";
+  cout << min << endl;
+  cout << "The max marks is ";
+  cout << max << endl;
+}
