eecce971c5c8321afdb3be639918f6d4b151c5a8
rohin-arka <surya_siwakoti@outlook.com>
2017-04-04 21:29:59 +0545

    completed q 1 and q 2

---
+++ ppa_03_set_1/q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(void){
+  int a = 2, b = 5, expr_one, expr_two;
+
+  expr_one = 5 * a - 2 * b;
+
+  expr_two = (4 * a + 8 * b) % (b - 1);
+
+  while((expr_one = expr_two)){
+    cout << "loop 1" << endl;
+  }
+
+  return 0;
+}
---
+++ ppa_03_set_1/q2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void){
+  int FACT = 1;
+  for(unsigned int i = 6; i >= 0; --i){
+    FACT *=1;
+  }
+  cout << FACT << endl;
+  return 0;
+}
