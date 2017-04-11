5673788846ee1de1e3ac495af8d37dc5c609105a
rohin-arka <surya_siwakoti@outlook.com>
2017-04-11 21:04:04 +0545

    completed quiz 8

---
+++ graded_quizes/quiz_8.cpp
@@ -0,0 +1,26 @@
+int main () {
+    int a = 6;
+    int num1, num2;
+    num1 = 672;
+    num2 = 54;
+
+    for ( ; ; ) {
+       a = a * 3;
+       if(a > num1) {
+          a = a - 5;
+          break;
+        }
+    }
+
+    while(true) {
+        a = a / 3;
+        if(a < num2) {
+            a = a - 5;
+            break;
+        }
+    }
+    cout << a;
+    return 0;
+}
+
+Answer: 48
