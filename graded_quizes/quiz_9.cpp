75b68ccaf9f60b087f3fa9baa0f7e4a27135401f
rohin-arka <surya_siwakoti@outlook.com>
2017-04-13 21:48:17 +0545

    completed lcm finding algorithm question

--- graded_quizes/quiz_9.cpp
+++ graded_quizes/quiz_9.cpp
@@ -1,19 +1,23 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int c, a, k, h, g;
-    cout << "Enter two numbers:";
-    cin >> c >> a;
-    k = c;
-    h = a;
-
-     while((c >= c)){
-        if(c > a)
-            g = k / g;
-        else
-            a = a - c;
-    }
-    a = c * c;
-    cout << g << endl;
-    return 0;
+  int c, a, k, h, g;
+  cout << "Enter two numbers:";
+  cin >> c >> a;
+  k = c;
+  h = a;
+  while(c != a){
+    if(c > a)
+      c = c - a;
+    else
+      a = a - c;
+  }
+  g = k * h / c;
+  cout << g << endl;
+  return 0;
 }
+// ANSWER
+// If we replace STATEMENT 1 with c != a and
+// STATEMENT 2 with c = c - a; and
+// STATEMENT 3 with g = k * h / c; ,
+// then the program computes LCM of the two numbers (c and a) and stores the result in g
