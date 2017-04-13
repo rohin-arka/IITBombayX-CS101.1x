44ca6672bb7f3076c59727f660776891439a6b6a
rohin-arka <surya_siwakoti@outlook.com>
2017-04-13 12:00:33 +0545

    completed quiz 10

--- graded_quizes/quiz_10.cpp
+++ graded_quizes/quiz_10.cpp
@@ -1,53 +1,35 @@
 #include <iostream>
 using namespace std;
 int main(void){
-  int inputNumber = 7024, number, ssd, inputNumber2, inputNumber3;
-
-  int counter2, counter, squareSum, lastTwo, additionLastTwo;
-  inputNumber3 = inputNumber;
-
-  for(int i = 0; i < 11; i++){
-    inputNumber2 = inputNumber;
-    int reverseNumber;
-    int newNumber1 = inputNumber2 / 10;
-    counter2 = 0;
-    counter = 1;
-    squareSum = 0;
-    lastTwo = 0;
-    while(inputNumber > 0){
-      number = inputNumber % 10;
-      squareSum += (number * number);
+  int inputNumber = 7345, ssd, inputLastNumber, rpm, rightShiftNumber, lastTwo, nextRpm, firstRpm;
+  firstRpm = inputNumber;
+  int year;
+  for(int i = 1; i < 11; i++){
+    ssd = 0, inputLastNumber = 0, rightShiftNumber = 0, nextRpm = 0;
+    rpm = inputNumber;
+    while(inputNumber !=0 ){
+      inputLastNumber = inputNumber % 10;
+      ssd += (inputLastNumber * inputLastNumber);
       inputNumber = inputNumber /10;
     }
-    ssd = squareSum * 323;
-    int newNumber = inputNumber2 % 10;
-    while(inputNumber2 != 0){
-      newNumber = newNumber * 10;
-      counter2 = newNumber;
-      inputNumber2 = inputNumber2 / 10;
-     }
-     cout << newNumber1 << endl;
-     reverseNumber = newNumber1 + counter2/10;
-     for(int i = 1; i < 3; i++){
-       if(i == 2){
-         lastTwo += (reverseNumber % 10)* 10;
-       }
-       else{
-         lastTwo = reverseNumber % 10;
-       }
-       reverseNumber = reverseNumber / 10;
-     }
-     additionLastTwo = ssd + lastTwo;
-     if((inputNumber3 * 8) < additionLastTwo){
-       break;
-     }
-     else{
-       inputNumber = additionLastTwo;
-     }
-
-    counter++;
+    ssd = ssd * 323;
+    inputLastNumber = rpm % 10;
+    rightShiftNumber = rpm/10;
+    while(rpm!=0){
+      inputLastNumber = (inputLastNumber * 10);
+      rpm = rpm / 10;
+    }
+    rightShiftNumber = inputLastNumber/10 + rightShiftNumber;
+    lastTwo = (rightShiftNumber % 10) + ((rightShiftNumber /10) % 10)*10;
+    nextRpm = ssd + lastTwo;
+    if((firstRpm * 8) < nextRpm){
+      year = i;
+      break;
+    }
+    else{
+      inputNumber = nextRpm;
+    }
   }
-  cout << lastTwo << endl;
-  cout << squareSum << endl;
+  cout << nextRpm << endl;
   return 0;
 }
