ff6c0b228460dd80edaf45ed2ad6321e74713f8b
rohin-arka <surya_siwakoti@outlook.com>
2017-04-18 21:00:22 +0545

    completed recursion exercise 7

---
+++ pps_04/q7.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+void t ( int n, char fp, char tp, char ap ) {
+  if ( n ==1) {
+    cout << fp << tp;
+    return;
+  }
+  t (n -1, fp, ap, tp);
+  cout << fp << tp;
+  t (n -1, ap, tp, fp);
+  return;
+
+}
+int main () {
+  t (2, 'x', 'y', 'z');
+  return 0;
+}
+// Answer: xzxyzy
